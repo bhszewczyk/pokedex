b03cf572dc369232ab8942e655d0607542f05a4e
Barbara Szewczyk <comamysza@gmail.com>
2022-10-25 19:02:34 +0200

    Refactors code

--- script.js
+++ script.js
@@ -36,22 +36,31 @@ for (const color in colors) {
 	startingOptionEl.append(option);
 }
 
+// get data for each pokemon from 1st gen
 async function fetch1stGen() {
-	resetPokedex();
 	for (let i = 1; i <= COUNT_1_GEN; i++) {
 		await getPokemon(i);
 	}
 }
 
+// fetch pokemon data using id
 async function getPokemon(id) {
+	// check if its already in the local Storage
+	if (localStorage.getItem(id)) {
+		return;
+	}
+
+	// if not already in the local storage - fetch data
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 	const results = await fetch(url);
 	const data = await results.json();
 
-	cashToLocalStorage(data);
+	// cache to the local storage
+	cacheToLocalStorage(data);
 }
 
-function cashToLocalStorage(data) {
+// extract data needed and cache it to the local storage
+function cacheToLocalStorage(data) {
 	const { id, name, forms, sprites, types } = data;
 
 	const pokemonData = {
@@ -61,9 +70,11 @@ function cashToLocalStorage(data) {
 		sprite: sprites.front_default,
 		types: types,
 	};
+
 	localStorage.setItem(id, JSON.stringify(pokemonData));
 }
 
+// get cached data
 function createPokeData() {
 	let allPokemon = [];
 
@@ -75,20 +86,20 @@ function createPokeData() {
 	return allPokemon;
 }
 
+// reset html
 function resetPokedex() {
 	pokedex.innerHTML = '';
 }
 
+// prepare data for only chosen pokemon
 async function filterPokemon(e) {
 	resetPokedex();
 	const clickedType = e.target.innerText;
 
-	let filteredPoke = [];
-
-	all1stGenPokemon.forEach((obj) => {
+	const filteredPoke = all1stGenPokemon.filter((obj) => {
 		for (let i = 0; i < obj.types.length; i++) {
 			if (obj.types[i].type.name === clickedType) {
-				filteredPoke.push(obj);
+				return obj;
 			}
 		}
 	});
@@ -96,12 +107,15 @@ async function filterPokemon(e) {
 	filteredPoke.forEach((poke) => createPokemonCard(poke));
 }
 
+// create card for chosen pokemon
 function createPokemonCard(pokemon) {
+	// create card div element
 	const cardEl = document.createElement('div');
+
+	// set styling
 	cardEl.classList.add('pokemon');
 
-	console.log(pokemon);
-
+	// get pokemon types
 	const pokeTypes = pokemon.types.map(
 		(type) =>
 			`<span style="background-color: ${colors[type.type.name]}">${
@@ -109,8 +123,7 @@ function createPokemonCard(pokemon) {
 			}</span>`
 	);
 
-	console.log(pokeTypes);
-
+	// create card html template
 	const cardTemplate = `
 	   <div class="img-container">
 			<img
@@ -131,14 +144,20 @@ function createPokemonCard(pokemon) {
 		</div>
 	`;
 
+	// set html for the card
 	cardEl.innerHTML = cardTemplate;
 
+	// add cardEl to DOM
 	pokedex.append(cardEl);
 }
 
+// create card for each pokemon from 1st generation
 function showAll1stGen() {
+	resetPokedex();
 	all1stGenPokemon.forEach((poke) => createPokemonCard(poke));
 }
 
+// fetch data for the first time
 fetch1stGen();
+// create global variablewith pokemon data
 const all1stGenPokemon = createPokeData();
